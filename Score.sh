9ed260e4902c6dc78680b4cc4b23a3757dbed9d8
Silence <silence1874@foxmail.com>
2021-10-17 09:36:06 +0800

    Score

---
+++ Score.sh
@@ -0,0 +1,13 @@
+silence@silence-vm:~/xv6-labs-2021$ ./grade-lab-util 
+make: 'kernel/kernel' is up to date.
+== Test sleep, no arguments == sleep, no arguments: OK (1.4s) 
+== Test sleep, returns == sleep, returns: OK (1.0s) 
+== Test sleep, makes syscall == sleep, makes syscall: OK (1.0s) 
+== Test pingpong == pingpong: OK (1.0s) 
+== Test primes == primes: OK (1.1s) 
+== Test find, in current directory == find, in current directory: OK (1.0s) 
+== Test find, recursive == find, recursive: OK (1.5s) 
+== Test xargs == xargs: OK (1.3s) 
+== Test time == 
+time: OK 
+Score: 100/100

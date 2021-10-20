c865d6bd14c76f575d09e20716df4abd4566a90c
Silence <silence1874@foxmail.com>
2021-10-20 10:36:27 +0800

    Score

---
+++ Score.sh
@@ -0,0 +1,10 @@
+silence@silence-vm:~/xv6-labs-2021$ ./grade-lab-syscall 
+make: 'kernel/kernel' is up to date.
+== Test trace 32 grep == trace 32 grep: OK (1.4s) 
+== Test trace all grep == trace all grep: OK (0.9s) 
+== Test trace nothing == trace nothing: OK (0.9s) 
+== Test trace children == trace children: OK (26.1s) 
+== Test sysinfotest == sysinfotest: OK (4.3s) 
+== Test time == 
+time: OK 
+Score: 35/35
